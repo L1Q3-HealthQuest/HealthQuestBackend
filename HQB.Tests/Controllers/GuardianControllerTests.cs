80778e56ae448c689ce87e68bae3207d32475b7b
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-03-25 18:03:40 +0100

    Add unit tests for Guardian and Patient controllers

---
+++ HQB.Tests/Controllers/GuardianControllerTests.cs
@@ -0,0 +1,108 @@
+using Moq;
+using HQB.WebApi.Models;
+using HQB.WebApi.Interfaces;
+using HQB.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HQB.Tests.Controllers
+{
+    [TestClass]
+    public class GuardianControllerTests
+    {
+        public required Mock<IGuardianRepository> _mockRepo;
+        public required GuardianController _controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockRepo = new Mock<IGuardianRepository>();
+            _controller = new GuardianController(_mockRepo.Object);
+        }
+
+        [TestMethod]
+        public async Task GetOuderVoogd_ReturnsOkResult_WithGuardians()
+        {
+            // Arrange
+            var guardians = new List<Guardian> { new() { ID = Guid.NewGuid(), FirstName = "John", LastName = "Doe", UserID = Guid.NewGuid().ToString() } };
+            _mockRepo.Setup(repo => repo.GetAllGuardiansAsync()).ReturnsAsync(guardians);
+
+            // Act
+            var result = await _controller.GetOuderVoogd();
+
+            // Assert
+            Assert.IsInstanceOfType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(guardians, okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task GetOuderVoogdById_GuardianExists_ReturnsOk()
+        {
+            // Arrange
+            var guardian = new Guardian { ID = Guid.NewGuid(), FirstName = "Jane", LastName = "Doe", UserID = Guid.NewGuid().ToString() };
+            _mockRepo.Setup(repo => repo.GetGuardianByIdAsync(guardian.ID)).ReturnsAsync(guardian);
+
+            // Act
+            var result = await _controller.GetOuderVoogdById(guardian.ID);
+
+            // Assert
+            Assert.IsInstanceOfType<OkObjectResult>(result);
+        }
+
+        [TestMethod]
+        public async Task GetOuderVoogdById_GuardianNotFound_ReturnsNotFound()
+        {
+            // Arrange
+            _mockRepo.Setup(repo => repo.GetGuardianByIdAsync(It.IsAny<Guid>())).ReturnsAsync((Guardian?)null);
+
+            // Act
+            var result = await _controller.GetOuderVoogdById(Guid.NewGuid());
+
+            // Assert
+            Assert.IsInstanceOfType<NotFoundResult>(result);
+        }
+
+        [TestMethod]
+        public async Task PostOuderVoogd_ValidGuardian_ReturnsCreated()
+        {
+            // Arrange
+            var guardian = new Guardian { ID = Guid.NewGuid(), FirstName = "First", LastName = "Last", UserID = Guid.NewGuid().ToString() };
+            _mockRepo.Setup(repo => repo.AddGuardianAsync(guardian)).ReturnsAsync(1);
+
+            // Act
+            var result = await _controller.PostOuderVoogd(guardian);
+
+            // Assert
+            Assert.IsInstanceOfType<CreatedAtActionResult>(result);
+        }
+
+        [TestMethod]
+        public async Task PutOuderVoogd_ValidGuardian_ReturnsNoContent()
+        {
+            // Arrange
+            var guardian = new Guardian { ID = Guid.NewGuid(), FirstName = "Updated", LastName = "Guardian", UserID = Guid.NewGuid().ToString() };
+            _mockRepo.Setup(repo => repo.UpdateGuardianAsync(guardian)).ReturnsAsync(1);
+
+            // Act
+            var result = await _controller.PutOuderVoogd(guardian.ID, guardian);
+
+            // Assert
+            Assert.IsInstanceOfType<NoContentResult>(result);
+        }
+
+        [TestMethod]
+        public async Task DeleteOuderVoogd_GuardianExists_ReturnsNoContent()
+        {
+            // Arrange
+            var guardianId = Guid.NewGuid();
+            _mockRepo.Setup(repo => repo.DeleteGuardianAsync(guardianId)).ReturnsAsync(1);
+
+            // Act
+            var result = await _controller.DeleteOuderVoogd(guardianId);
+
+            // Assert
+            Assert.IsInstanceOfType<NoContentResult>(result);
+        }
+    }
+}
---
+++ HQB.Tests/Controllers/PatientControllerTests.cs
@@ -0,0 +1,122 @@
+using Moq;
+using HQB.WebApi.Models;
+using HQB.WebApi.Interfaces;
+using HQB.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HQB.Tests.Controllers
+{
+    [TestClass]
+    public class PatientControllerTest
+    {
+        public required Mock<IPatientRepository> _mockRepo;
+        public required PatientController _controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockRepo = new Mock<IPatientRepository>();
+            _controller = new PatientController(_mockRepo.Object);
+        }
+
+        [TestMethod]
+        public async Task GetAllPatients_ReturnsOkResult_WithListOfPatients()
+        {
+            // Arrange
+            var patients = new List<Patient> { new() { ID = Guid.NewGuid(), FirstName = "John", LastName = "Doe", Avatar = "defaultAvatar.png" } };
+            _mockRepo.Setup(repo => repo.GetAllPatientsAsync()).ReturnsAsync(patients);
+
+            // Act
+            var result = await _controller.GetAllPatients();
+
+            // Assert
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+            Assert.AreEqual(patients, okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task GetPatientById_ReturnsOkResult_WithPatient()
+        {
+            // Arrange
+            var patient = new Patient { ID = Guid.NewGuid(), FirstName = "John", LastName = "Doe", Avatar = "defaultAvatar.png" };
+            _mockRepo.Setup(repo => repo.GetPatientByIdAsync(It.IsAny<int>())).ReturnsAsync(patient);
+
+            // Act
+            var result = await _controller.GetPatientById(1);
+
+            // Assert
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+            Assert.AreEqual(patient, okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task GetPatientById_ReturnsNotFoundResult_WhenPatientNotFound()
+        {
+            // Arrange
+            _mockRepo.Setup(repo => repo.GetPatientByIdAsync(It.IsAny<int>())).ReturnsAsync((Patient?)null);
+
+            // Act
+            var result = await _controller.GetPatientById(1);
+
+            // Assert
+            var notFoundResult = result as NotFoundResult;
+            Assert.IsNotNull(notFoundResult);
+            Assert.AreEqual(404, notFoundResult.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task AddPatient_ReturnsCreatedAtActionResult_WithPatient()
+        {
+            // Arrange
+            var patient = new Patient { ID = Guid.NewGuid(), FirstName = "John", LastName = "Doe", Avatar = "defaultAvatar.png" };
+            _mockRepo.Setup(repo => repo.AddPatientAsync(It.IsAny<Patient>())).ReturnsAsync(1);
+
+            // Act
+            var result = await _controller.AddPatient(patient);
+
+            // Assert
+            var createdAtActionResult = result as CreatedAtActionResult;
+            Assert.IsNotNull(createdAtActionResult);
+            Assert.AreEqual(201, createdAtActionResult.StatusCode);
+            Assert.AreEqual(patient, createdAtActionResult.Value);
+        }
+
+        [TestMethod]
+        public async Task UpdatePatient_ReturnsNoContentResult()
+        {
+            // Arrange
+            var patient = new Patient { ID = Guid.NewGuid(), FirstName = "John", LastName = "Doe", Avatar = "defaultAvatar.png" };
+            _mockRepo.Setup(repo => repo.GetPatientByIdAsync(It.IsAny<int>())).ReturnsAsync(patient);
+            _mockRepo.Setup(repo => repo.UpdatePatientAsync(It.IsAny<Patient>())).ReturnsAsync(1);
+
+            // Act
+            var result = await _controller.UpdatePatient(1, patient);
+
+            // Assert
+            var noContentResult = result as NoContentResult;
+            Assert.IsNotNull(noContentResult);
+            Assert.AreEqual(204, noContentResult.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task DeletePatient_ReturnsNoContentResult()
+        {
+            // Arrange
+            var patient = new Patient { ID = Guid.NewGuid(), FirstName = "John", LastName = "Doe", Avatar = "defaultAvatar.png" };
+            _mockRepo.Setup(repo => repo.GetPatientByIdAsync(It.IsAny<int>())).ReturnsAsync(patient);
+            _mockRepo.Setup(repo => repo.DeletePatientAsync(It.IsAny<int>())).ReturnsAsync(1);
+
+            // Act
+            var result = await _controller.DeletePatient(1);
+
+            // Assert
+            var noContentResult = result as NoContentResult;
+            Assert.IsNotNull(noContentResult);
+            Assert.AreEqual(204, noContentResult.StatusCode);
+        }
+    }
+}
