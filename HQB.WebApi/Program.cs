9ffbd7df29f8cb045e8a13cdcd6077cfc38077a4
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-03-25 15:27:41 +0100

    Add Identity Framework services and configure API endpoints for user management

--- HQB.WebApi/Program.cs
+++ HQB.WebApi/Program.cs
@@ -1,23 +1,33 @@
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var dbConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Add Identity Framework services
+var sqlConnectionString = builder.Configuration.GetConnectionString("SQLConnection");
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+    options.Password.RequiredLength = 10;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+})
+.AddRoles<IdentityRole>()
+.AddDapperStores(options => options.ConnectionString = sqlConnectionString);
+
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
+app.MapGroup("/account").MapIdentityApi<IdentityUser>();
+app.MapControllers().RequireAuthorization();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
-app.MapControllers();
-
+app.MapOpenApi();
 app.Run();
