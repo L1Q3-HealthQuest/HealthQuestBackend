5f116be25eed3c7bcb79eb162a70a82f45aaf355
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-03-25 21:26:45 +0100

    Update JournalController route to version 1

--- HQB.WebApi/Controllers/JournalController.cs
+++ HQB.WebApi/Controllers/JournalController.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Logging;
 
 namespace HQB.WebApi.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/v1/[controller]")]
 [ApiController]
 public class JournalController : ControllerBase
 {
