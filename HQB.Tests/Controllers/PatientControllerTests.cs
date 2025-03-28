e42fe82a93b7c8b8626f74fb7d1085d139ab3247
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-03-28 19:39:24 +0100

    Enhance test coverage of PatientController methodes

--- HQB.Tests/Controllers/PatientControllerTests.cs
+++ HQB.Tests/Controllers/PatientControllerTests.cs
@@ -3,59 +3,62 @@ using HQB.WebApi.Models;
 using HQB.WebApi.Interfaces;
 using HQB.WebApi.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
 namespace HQB.Tests.Controllers
 {
     [TestClass]
-    public class PatientControllerTest
+    public class PatientControllerTests
     {
         public required PatientController _controller;
-        public required Mock<IPatientRepository> _mockRepo;
-        public required Mock<IDoctorRepository> _mockDoctorRepo;
-        public required Mock<IJournalRepository> _mockJournalRepo;
-        public required Mock<IGuardianRepository> _mockGuardianRepo;
-        public required Mock<IStickersRepository> _mockStickersRepo;
         public required Mock<ILogger<PatientController>> _mockLogger;
-        public required Mock<ITreatmentRepository> _mockTreatmentRepo;
-        public required Mock<IAuthenticationService> _mockAuthService;
-        public required Mock<ICompletedAppointmentsRepository> _mockCompletedAppointmentsRepo;
+        public required Mock<IDoctorRepository> _mockDoctorRepository;
+        public required Mock<IPatientRepository> _mockPatientRepository;
+        public required Mock<IJournalRepository> _mockJournalRepository;
+        public required Mock<IStickersRepository> _mockStickersRepository;
+        public required Mock<IGuardianRepository> _mockGuardianRepository;
+        public required Mock<ITreatmentRepository> _mockTreatmentRepository;
+        public required Mock<IAuthenticationService> _mockAuthenticationService;
+        public required Mock<ICompletedAppointmentsRepository> _mockCompletedAppointmentsRepository;
 
         [TestInitialize]
         public void Setup()
         {
-            _mockRepo = new Mock<IPatientRepository>();
-            _mockDoctorRepo = new Mock<IDoctorRepository>();
-            _mockJournalRepo = new Mock<IJournalRepository>();
-            _mockGuardianRepo = new Mock<IGuardianRepository>();
-            _mockStickersRepo = new Mock<IStickersRepository>();
             _mockLogger = new Mock<ILogger<PatientController>>();
-            _mockTreatmentRepo = new Mock<ITreatmentRepository>();
-            _mockAuthService = new Mock<IAuthenticationService>();
-            _mockCompletedAppointmentsRepo = new Mock<ICompletedAppointmentsRepository>();
+            _mockDoctorRepository = new Mock<IDoctorRepository>();
+            _mockPatientRepository = new Mock<IPatientRepository>();
+            _mockJournalRepository = new Mock<IJournalRepository>();
+            _mockStickersRepository = new Mock<IStickersRepository>();
+            _mockGuardianRepository = new Mock<IGuardianRepository>();
+            _mockTreatmentRepository = new Mock<ITreatmentRepository>();
+            _mockAuthenticationService = new Mock<IAuthenticationService>();
+            _mockCompletedAppointmentsRepository = new Mock<ICompletedAppointmentsRepository>();
 
             _controller = new PatientController(
                 _mockLogger.Object,
-                _mockDoctorRepo.Object,
-                _mockRepo.Object,
-                _mockJournalRepo.Object,
-                _mockStickersRepo.Object,
-                _mockGuardianRepo.Object,
-                _mockTreatmentRepo.Object,
-                _mockAuthService.Object,
-                _mockCompletedAppointmentsRepo.Object
+                _mockDoctorRepository.Object,
+                _mockPatientRepository.Object,
+                _mockJournalRepository.Object,
+                _mockStickersRepository.Object,
+                _mockGuardianRepository.Object,
+                _mockTreatmentRepository.Object,
+                _mockAuthenticationService.Object,
+                _mockCompletedAppointmentsRepository.Object
             );
         }
 
         [TestMethod]
-        public async Task GetAllPatients_ReturnsOkResult_WithListOfPatients()
+        public async Task GetPatientsForCurrentUser_ReturnsOk_WhenPatientsExist()
         {
             // Arrange
-            var patients = new List<Patient> { new() { ID = Guid.NewGuid(), FirstName = "John", LastName = "Doe", Avatar = "defaultAvatar.png" } };
-            _mockRepo.Setup(repo => repo.GetPatientsByGuardianId(It.IsAny<Guid>())).ReturnsAsync(patients);
-            var guardian = new Guardian { ID = Guid.NewGuid(), FirstName = "Jane", LastName = "Doe" };
-            _mockAuthService.Setup(service => service.GetCurrentAuthenticatedUserId()).Returns(Guid.NewGuid().ToString());
-            _mockGuardianRepo.Setup(repo => repo.GetGuardianByUserIdAsync(It.IsAny<string>())).ReturnsAsync(guardian);
+            var userId = "test-user-id";
+            var guardian = new Guardian { ID = Guid.NewGuid(), FirstName = "DefaultFirstName", LastName = "DefaultLastName" };
+            var patients = new List<Patient> { new Patient { ID = Guid.NewGuid(), FirstName = "John", LastName = "Doe", Avatar = "default-avatar.png" } };
+
+            _mockAuthenticationService.Setup(s => s.GetCurrentAuthenticatedUserId()).Returns(userId);
+            _mockGuardianRepository.Setup(r => r.GetGuardianByUserIdAsync(userId)).ReturnsAsync(guardian);
+            _mockPatientRepository.Setup(r => r.GetPatientsByGuardianId(guardian.ID)).ReturnsAsync(patients);
 
             // Act
             var result = await _controller.GetPatientsForCurrentUser();
@@ -63,96 +66,116 @@ namespace HQB.Tests.Controllers
             // Assert
             var okResult = result.Result as OkObjectResult;
             Assert.IsNotNull(okResult);
-            Assert.AreEqual(200, okResult.StatusCode);
+            Assert.AreEqual(StatusCodes.Status200OK, okResult.StatusCode);
             Assert.AreEqual(patients, okResult.Value);
         }
 
         [TestMethod]
-        public async Task GetPatientById_ReturnsOkResult_WithPatient()
+        public async Task GetPatientsForCurrentUser_ReturnsBadRequest_WhenUserIdIsNull()
         {
             // Arrange
-            var patient = new Patient { ID = Guid.NewGuid(), FirstName = "John", LastName = "Doe", Avatar = "defaultAvatar.png" };
-            _mockRepo.Setup(repo => repo.GetPatientByIdAsync(It.IsAny<Guid>())).ReturnsAsync(patient);
+            _mockAuthenticationService.Setup(s => s.GetCurrentAuthenticatedUserId()).Returns((string)null!);
 
             // Act
-            var result = await _controller.GetPatientById(Guid.NewGuid());
+            var result = await _controller.GetPatientsForCurrentUser();
+
+            // Assert
+            var badRequestResult = result.Result as BadRequestObjectResult;
+            Assert.IsNotNull(badRequestResult);
+            Assert.AreEqual(StatusCodes.Status400BadRequest, badRequestResult.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task GetPatientById_ReturnsOk_WhenPatientExists()
+        {
+            // Arrange
+            var patientId = Guid.NewGuid();
+            var patient = new Patient { ID = patientId, FirstName = "John", LastName = "Doe", Avatar = "default-avatar.png" };
+
+            _mockPatientRepository.Setup(r => r.GetPatientByIdAsync(patientId)).ReturnsAsync(patient);
+
+            // Act
+            var result = await _controller.GetPatientById(patientId);
 
             // Assert
             var okResult = result.Result as OkObjectResult;
             Assert.IsNotNull(okResult);
-            Assert.AreEqual(200, okResult.StatusCode);
+            Assert.AreEqual(StatusCodes.Status200OK, okResult.StatusCode);
             Assert.AreEqual(patient, okResult.Value);
         }
 
         [TestMethod]
-        public async Task GetPatientById_ReturnsNotFoundResult_WhenPatientNotFound()
+        public async Task GetPatientById_ReturnsNotFound_WhenPatientDoesNotExist()
         {
             // Arrange
-            _mockRepo.Setup(repo => repo.GetPatientByIdAsync(It.IsAny<Guid>())).ReturnsAsync((Patient?)null);
+            var patientId = Guid.NewGuid();
+
+            _mockPatientRepository.Setup(r => r.GetPatientByIdAsync(patientId)).ReturnsAsync((Patient)null!);
 
             // Act
-            var result = await _controller.GetPatientById(Guid.NewGuid());
+            var result = await _controller.GetPatientById(patientId);
 
             // Assert
             var notFoundResult = result.Result as NotFoundResult;
             Assert.IsNotNull(notFoundResult);
-            Assert.AreEqual(404, notFoundResult.StatusCode);
+            Assert.AreEqual(StatusCodes.Status404NotFound, notFoundResult.StatusCode);
         }
 
         [TestMethod]
-        public async Task AddPatient_ReturnsCreatedAtActionResult_WithPatient()
+        public async Task AddPatient_ReturnsCreated_WhenPatientIsAdded()
         {
             // Arrange
-            var patient = new Patient { ID = Guid.NewGuid(), FirstName = "John", LastName = "Doe", Avatar = "defaultAvatar.png" };
-            var guardian = new Guardian { ID = Guid.NewGuid(), FirstName = "Jane", LastName = "Doe" };
-            _mockRepo.Setup(repo => repo.AddPatientAsync(It.IsAny<Patient>())).ReturnsAsync(1);
-            _mockGuardianRepo.Setup(repo => repo.GetGuardianByIdAsync(It.IsAny<Guid>())).ReturnsAsync(guardian);
-            _mockGuardianRepo.Setup(repo => repo.GetGuardianByUserIdAsync(It.IsAny<string>())).ReturnsAsync(guardian); // <-- Add this line
-            _mockAuthService.Setup(service => service.GetCurrentAuthenticatedUserId()).Returns(Guid.NewGuid().ToString());
-            _mockRepo.Setup(repo => repo.GetPatientByIdAsync(It.IsAny<Guid>())).ReturnsAsync(patient);
+            var patient = new Patient { FirstName = "John", LastName = "Doe", Avatar = "default-avatar.png" };
+            var userId = "test-user-id";
+            var guardian = new Guardian { ID = Guid.NewGuid(), FirstName = "DefaultFirstName", LastName = "DefaultLastName" };
+
+            _mockAuthenticationService.Setup(s => s.GetCurrentAuthenticatedUserId()).Returns(userId);
+            _mockGuardianRepository.Setup(r => r.GetGuardianByUserIdAsync(userId)).ReturnsAsync(guardian);
+            _mockPatientRepository.Setup(r => r.AddPatientAsync(It.IsAny<Patient>())).Returns(Task.FromResult(1));
 
             // Act
             var result = await _controller.AddPatient(patient);
 
             // Assert
-            var createdAtActionResult = result.Result as CreatedAtActionResult;
-            Assert.IsNotNull(createdAtActionResult);
-            Assert.AreEqual(201, createdAtActionResult.StatusCode);
-            Assert.AreEqual(patient, createdAtActionResult.Value);
+            var createdResult = result.Result as CreatedAtActionResult;
+            Assert.IsNotNull(createdResult);
+            Assert.AreEqual(StatusCodes.Status201Created, createdResult.StatusCode);
         }
 
         [TestMethod]
-        public async Task UpdatePatient_ReturnsNoContentResult()
+        public async Task DeletePatient_ReturnsNoContent_WhenPatientIsDeleted()
         {
             // Arrange
-            var patient = new Patient { ID = Guid.NewGuid(), FirstName = "John", LastName = "Doe", Avatar = "defaultAvatar.png" };
-            _mockRepo.Setup(repo => repo.GetPatientByIdAsync(It.IsAny<Guid>())).ReturnsAsync(patient);
-            _mockRepo.Setup(repo => repo.UpdatePatientAsync(It.IsAny<Patient>())).ReturnsAsync(1);
+            var patientId = Guid.NewGuid();
+            var patient = new Patient { ID = patientId, FirstName = "DefaultFirstName", LastName = "DefaultLastName", Avatar = "default-avatar.png" };
+
+            _mockPatientRepository.Setup(r => r.GetPatientByIdAsync(patientId)).ReturnsAsync(patient);
+            _mockPatientRepository.Setup(r => r.DeletePatientAsync(patientId)).ReturnsAsync(1);
 
             // Act
-            var result = await _controller.UpdatePatient(Guid.NewGuid(), patient);
+            var result = await _controller.DeletePatient(patientId);
 
             // Assert
             var noContentResult = result as NoContentResult;
             Assert.IsNotNull(noContentResult);
-            Assert.AreEqual(204, noContentResult.StatusCode);
+            Assert.AreEqual(StatusCodes.Status204NoContent, noContentResult.StatusCode);
         }
 
         [TestMethod]
-        public async Task DeletePatient_ReturnsNoContentResult()
+        public async Task DeletePatient_ReturnsNotFound_WhenPatientDoesNotExist()
         {
             // Arrange
-            var patient = new Patient { ID = Guid.NewGuid(), FirstName = "John", LastName = "Doe", Avatar = "defaultAvatar.png" };
-            _mockRepo.Setup(repo => repo.GetPatientByIdAsync(It.IsAny<Guid>())).ReturnsAsync(patient);
-            _mockRepo.Setup(repo => repo.DeletePatientAsync(It.IsAny<Guid>())).ReturnsAsync(1);
+            var patientId = Guid.NewGuid();
+
+            _mockPatientRepository.Setup(r => r.GetPatientByIdAsync(patientId)).ReturnsAsync((Patient)null!);
 
             // Act
-            var result = await _controller.DeletePatient(Guid.NewGuid());
+            var result = await _controller.DeletePatient(patientId);
 
             // Assert
-            var noContentResult = result as NoContentResult;
-            Assert.IsNotNull(noContentResult);
-            Assert.AreEqual(204, noContentResult.StatusCode);
+            var notFoundResult = result as NotFoundResult;
+            Assert.IsNotNull(notFoundResult);
+            Assert.AreEqual(StatusCodes.Status404NotFound, notFoundResult.StatusCode);
         }
     }
 }
