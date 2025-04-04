09d37373986da76524c4a2a0e38e4bab36c5f263
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-04-04 21:28:24 +0200

    Make GuardianID, TreatmentID, and DoctorID properties nullable in Patient model

--- HQB.WebApi/Models/Patient.cs
+++ HQB.WebApi/Models/Patient.cs
@@ -22,17 +22,17 @@ public class Patient
     /// <summary>
     /// Gets or sets the unique identifier of the patient's guardian.
     /// </summary>
-    public Guid GuardianID { get; set; }
+    public Guid? GuardianID { get; set; }
 
     /// <summary>
     /// Gets or sets the unique identifier of the patient's treatment.
     /// </summary>
-    public Guid TreatmentID { get; set; }
+    public Guid? TreatmentID { get; set; }
 
     /// <summary>
     /// Gets or sets the unique identifier of the patient's doctor.
     /// </summary>
-    public Guid DoctorID { get; set; }
+    public Guid? DoctorID { get; set; }
 
     /// <summary>
     /// Gets or sets the URL or path to the patient's avatar image.
